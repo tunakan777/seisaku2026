927f8787a6710ae55a891c166cfe61e54583425a
miruku306 <mi.ma.2x9.28@gmail.com>
2026-05-29 11:09:55 +0900

    githubアカウント認証の画面

---
+++ mimamorukun/src/renderer/src/pages/page1.ts
@@ -0,0 +1,23 @@
+import { showPage } from '../utils/dom'
+
+export function setupPage1(): void {
+  const loginBtn = document.getElementById('loginBtn')
+  const userCodeText = document.getElementById('userCode')
+  const statusText = document.getElementById('authStatus')
+
+  loginBtn?.addEventListener('click', async () => {
+    if (statusText) statusText.innerText = '認証中...'
+
+    const { userCode } = await window.api.login()
+    if (userCodeText) {
+      userCodeText.innerText = `GitHubで以下のコードを入力してください: ${userCode}`
+    }
+
+    const token = await window.api.poll()
+    if (token) {
+      if (userCodeText) userCodeText.innerText = ''
+      if (statusText) statusText.innerText = ''
+      showPage('page2')
+    }
+  })
+}
